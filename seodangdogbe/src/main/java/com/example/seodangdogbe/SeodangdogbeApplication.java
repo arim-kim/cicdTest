a4b8a5a0b484c6dca5b7e8651d9bf463a6232d9e
arimkim <kal72222222@gmail.com>
2024-03-02 16:07:51 +0900

    clear init cache

---
+++ seodangdogbe/src/main/java/com/example/seodangdogbe/SeodangdogbeApplication.java
@@ -0,0 +1,14 @@
+package com.example.seodangdogbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeodangdogbeApplication {
+
+	// application
+	public static void main(String[] args) {
+		SpringApplication.run(SeodangdogbeApplication.class, args);
+	}
+
+}
