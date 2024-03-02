a587dda8efe684799112d93383aa53b7bde2c512
arimkim <kal72222222@gmail.com>
2024-03-02 11:51:57 +0900

    init

---
+++ seodangDogBe/src/main/java/com/example/seodangDogBe/SeodangDogBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.seodangDogBe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeodangDogBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeodangDogBeApplication.class, args);
+	}
+
+}
---
+++ seodangDogBe/src/test/java/com/example/seodangDogBe/SeodangDogBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seodangDogBe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeodangDogBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
