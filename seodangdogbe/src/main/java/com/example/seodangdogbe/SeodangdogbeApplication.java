bca3f51047ea9c2b598473ec5ae5fe65ed0f1f08
arimkim <kal72222222@gmail.com>
2024-03-02 16:08:38 +0900

    test: git check

--- seodangdogbe/src/main/java/com/example/seodangdogbe/SeodangdogbeApplication.java
+++ seodangdogbe/src/main/java/com/example/seodangdogbe/SeodangdogbeApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SeodangdogbeApplication {
 
-	// application
 	public static void main(String[] args) {
 		SpringApplication.run(SeodangdogbeApplication.class, args);
 	}
